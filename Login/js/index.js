e46fa156de3533bb0235572e3fd79e019fe6dc50
Robbinsvillebalmandal <49110816+Robbinsvillebalmandal@users.noreply.github.com>
2019-04-02 18:19:05 -0400

    Update index.js

--- Login/js/index.js
+++ Login/js/index.js
@@ -5,9 +5,6 @@ const USERNAME = "balmandal", PASSWORD = "Sadhan@19";
  $("#login-button").click(function(event){
 		 event.preventDefault();
 
-		 // var user = document.forms["form"]["Username"].value;
-		 // var pass = document.forms["form"]["Password"].value;
-
 		 var user = document.getElementById("username").value;
 		 var pass = document.getElementById("password").value;
 
