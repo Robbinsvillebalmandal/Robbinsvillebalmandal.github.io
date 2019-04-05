f11c6628c45c363a156fd8592c015c2ecad33760
Robbinsvillebalmandal <49110816+Robbinsvillebalmandal@users.noreply.github.com>
2019-04-05 18:54:26 -0400

    Update index.js

--- Login/js/index.js
+++ Login/js/index.js
@@ -1,6 +1,19 @@
  
-const USERNAME = "balmandal", PASSWORD = "Sadhan@19";
+const USERNAME = "791684674", PASSWORD = "-814546199";
 
+
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
 
  $("#login-button").click(function(event){
 		 event.preventDefault();
@@ -8,7 +21,7 @@ const USERNAME = "balmandal", PASSWORD = "Sadhan@19";
 		 var user = document.getElementById("username").value;
 		 var pass = document.getElementById("password").value;
 
-		 if(USERNAME == user && PASSWORD == pass) {
+		 if(USERNAME == user.hashCode() && PASSWORD == pass.hashCode()) {
 			  $('form').fadeOut(500);
 	 		$('.wrapper').addClass('form-success');
 			 
